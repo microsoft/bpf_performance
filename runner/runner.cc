adf951fc58a75ed5184b3984f61bf024d74f49fb
Alan Jowett <alanjo@microsoft.com>
2023-08-11 09:47:26 -0700

    Fix build (#4)
    * Fix build
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    * Remove depedency on CPUs
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    * Fix codeql
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    
    ---------
    
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    Co-authored-by: Alan Jowett <alan.jowett@microsoft.com>

--- runner/runner.cc
+++ runner/runner.cc
@@ -136,6 +136,17 @@ int main(int argc, char **argv)
                                 cpu_program_assignments[i] = {program_fd};
                             }
                         }
+                        else if (assignment.second.as<std::string>() == "unassigned")
+                        {
+                            // Assign program to all unassigned CPUs.
+                            for (size_t i = 0; i < cpu_program_assignments.size(); i++)
+                            {
+                                if (!cpu_program_assignments[i].has_value())
+                                {
+                                    cpu_program_assignments[i] = {program_fd};
+                                }
+                            }
+                        }
                         else
                         {
                             int cpu = assignment.second.as<int>();
