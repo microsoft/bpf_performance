6d9a94080efdba3627315db8aa5f0c692d0de497
Alan Jowett <alanjo@microsoft.com>
2023-08-15 14:58:15 -0700

    Add helper function perf test (#12)
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>
    Co-authored-by: Alan Jowett <alan.jowett@microsoft.com>

---
+++ bpf/helpers.c
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation
+// SPDX-License-Identifier: MIT
+
+#include "bpf.h"
+
+// Simplest possible program, just pass everything, used to measure the
+// overhead of the BPF infrastructure.
+
+__attribute__((section("xdp"))) int
+test_bpf_get_prandom_u32(void* ctx)
+{
+    int i = bpf_get_prandom_u32();
+    return 0;
+}
