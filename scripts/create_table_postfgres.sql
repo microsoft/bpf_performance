7143787323fe4639e556f337d8c6d7b591c95a10
Alan Jowett <alan.jowett@microsoft.com>
2023-09-14 14:02:35 -0700

    Update postgres script to create table
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- scripts/create_table_postfgres.sql
+++ scripts/create_table_postfgres.sql
@@ -5,7 +5,7 @@ CREATE TABLE BenchmarkResults (
     Timestamp TIMESTAMPTZ,
     Metric VARCHAR(255),
     Value NUMERIC,
-    CommitHash VARCHAR(40),
+    CommitHash VARCHAR(255),
     Platform VARCHAR(255),
     Repository VARCHAR(255),
 );
