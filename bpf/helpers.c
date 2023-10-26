80b0e2e394c0f0edac9953ffeb1315606a9c81ad
Alan Jowett <alan.jowett@microsoft.com>
2023-10-26 09:35:53 -0700

    Add additional helpers
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- bpf/helpers.c
+++ bpf/helpers.c
@@ -9,3 +9,21 @@ SEC("xdp/test_bpf_get_prandom_u32") int test_bpf_get_prandom_u32(void* ctx)
     int i = bpf_get_prandom_u32();
     return 0;
 }
+
+SEC("xdp/test_bpf_ktime_get_boot_ns") int test_bpf_ktime_get_boot_ns(void* ctx)
+{
+    uint64_t i = bpf_ktime_get_boot_ns();
+    return 0;
+}
+
+SEC("xdp/test_bpf_ktime_get_ns") int test_bpf_ktime_get_ns(void* ctx)
+{
+    uint64_t i = bpf_ktime_get_ns();
+    return 0;
+}
+
+SEC("xdp/test_bpf_get_smp_processor_id") int test_bpf_get_smp_processor_id(void* ctx)
+{
+    uint64_t i = bpf_get_smp_processor_id();
+    return 0;
+}
