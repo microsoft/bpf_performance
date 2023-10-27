9f7d3ea3912cf17e135c4d70431f5e0acba08fa2
Alan Jowett <alan.jowett@microsoft.com>
2023-10-27 15:41:14 -0700

    Measure perf of bpf_ringbuf_output
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

---
+++ bpf/ringbuf.c
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation
+// SPDX-License-Identifier: MIT
+
+#include "bpf.h"
+
+struct
+{
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 128 * 1024);
+} map SEC(".maps");
+
+SEC("xdp/bpf_ringbuf_output") int output(void* ctx)
+{
+    char msg[] = "Hello, World!";
+    return bpf_ringbuf_output(&map, msg, sizeof(msg), 0);
+}
