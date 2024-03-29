73c826d3f2aad08f193d254d0b2deece2d0f5f97
Alan Jowett <alan.jowett@microsoft.com>
2024-03-29 13:27:39 -0700

    Two machine test
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

---
+++ scripts/two-machine-perf.ps1
@@ -0,0 +1,57 @@
+# Copyright (c) Microsoft Corporation
+# SPDX-License-Identifier: MIT
+
+param (
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("Debug", "Release")]
+    [string]$Config = "Release",
+
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("x64")]
+    [string]$Arch = "x64",
+
+    [Parameter(Mandatory = $false)]
+    [string]$PeerName = "netperf-peer",
+
+    [Parameter(Mandatory = $false)]
+    [string]$RemotePSConfiguration = "PowerShell.7",
+
+    [Parameter(Mandatory = $false)]
+    [string]$RemoteDir = "C:\_work"
+)
+
+#Set-StrictMode -Version 'Latest'
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+#$ProgressPreference = 'SilentlyContinue'
+
+# Set up the connection to the peer over remote powershell.
+Write-Output "Connecting to $PeerName..."
+$Username = (Get-ItemProperty 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon').DefaultUserName
+$Password = (Get-ItemProperty 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon').DefaultPassword | ConvertTo-SecureString -AsPlainText -Force
+$Creds = New-Object System.Management.Automation.PSCredential ($Username, $Password)
+$Session = New-PSSession -ComputerName $PeerName -Credential $Creds -ConfigurationName $RemotePSConfiguration
+if ($null -eq $Session) {
+    Write-Error "Failed to create remote session"
+}
+
+# Find all the local and remote IP and MAC addresses.
+$RemoteAddress = [System.Net.Dns]::GetHostAddresses($Session.ComputerName)[0].IPAddressToString
+Write-Output "Successfully connected to peer: $RemoteAddress"
+
+Write-Output "Copying the tests to the remote machine"
+Copy-Item -ToSession $Session . -Destination $RemoteDir\cts-traffic -Recurse -Force
+
+Write-Output "Running the tests on the remote machine"
+$Job = Invoke-Command -Session $Session -ScriptBlock {
+    .\ctsTraffic.exe -listen:* -consoleverbosity:1 -timelimit:90000
+}
+
+.\ctsTraffic.exe -target:$RemoteAddress -consoleverbosity:1 -statusfilename:clientstatus.csv -connectionfilename:clientconnections.csv -timelimit:60000
+
+Write-Output "Waiting for the remote job to complete"
+Wait-Job $Job | Out-Null
+Receive-Job .\.clang-format -ErrorAction SilentlyContinue
+
+Write-Output "Tests completed. Cleaning up..."
+
+Remove-PSSession $Session
