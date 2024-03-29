ac51ccec9eb7887c4c07bebd25506d26df0d58f2
Gianni Trevisiol <gtrevi@users.noreply.github.com>
2024-03-29 15:01:34 -0700

    draft

--- bpf/ringbuf.c
+++ bpf/ringbuf.c
@@ -3,15 +3,23 @@
 
 #include "bpf.h"
 
+#if !defined(MAX_RECORDS)
+#define MAX_RECORDS 1024
+#endif
+
+#if !defined(RECORD_SIZE)
+#define RECORD_SIZE 128
+#endif
+
 struct
 {
     __uint(type, BPF_MAP_TYPE_RINGBUF);
-    __uint(max_entries, 128 * 1024);
+    __uint(max_entries, MAX_RECORDS * RECORD_SIZE);
 } map SEC(".maps");
 
 SEC("sockops/bpf_ringbuf_output") int output(void* ctx)
 {
-    char msg[] = "Hello, World!";
+    char msg[RECORD_SIZE] = {0};
     if (bpf_ringbuf_output(&map, msg, sizeof(msg), 0) < 0) {
         return 1;
     } else {
