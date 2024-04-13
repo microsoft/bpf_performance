8cd8a8951208939283cea1525e4198eba4cf80bb
Alan Jowett <alan.jowett@microsoft.com>
2024-04-13 10:45:51 -0700

    Remove dependency in incompatible xdp_md_t type
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- bpf/xdp.c
+++ bpf/xdp.c
@@ -3,11 +3,11 @@
 
 #include "bpf.h"
 
-SEC("xdp/baseline") int test_xdp_baseline(xdp_md_t* ctx) { return XDP_PASS; }
+SEC("xdp/baseline") int test_xdp_baseline(void* ctx) { return XDP_PASS; }
 
 // Test cases for bpf_xdp_adjust_head
 
-SEC("xdp/test_bpf_xdp_adjust_head_0") int test_bpf_xdp_adjust_head_0(xdp_md_t* ctx)
+SEC("xdp/test_bpf_xdp_adjust_head_0") int test_bpf_xdp_adjust_head_0(void* ctx)
 {
     if (bpf_xdp_adjust_head(ctx, 0) < 0) {
         return XDP_DROP;
@@ -15,7 +15,7 @@ SEC("xdp/test_bpf_xdp_adjust_head_0") int test_bpf_xdp_adjust_head_0(xdp_md_t* c
     return XDP_PASS;
 }
 
-SEC("xdp/test_bpf_xdp_adjust_head_plus_100") int test_bpf_xdp_adjust_head_plus_100(xdp_md_t* ctx)
+SEC("xdp/test_bpf_xdp_adjust_head_plus_100") int test_bpf_xdp_adjust_head_plus_100(void* ctx)
 {
     if (bpf_xdp_adjust_head(ctx, 100) < 0) {
         return XDP_DROP;
@@ -27,7 +27,7 @@ SEC("xdp/test_bpf_xdp_adjust_head_plus_100") int test_bpf_xdp_adjust_head_plus_1
     return XDP_PASS;
 }
 
-SEC("xdp/test_bpf_xdp_adjust_head_minus_100") int test_bpf_xdp_adjust_head_minus_100(xdp_md_t* ctx)
+SEC("xdp/test_bpf_xdp_adjust_head_minus_100") int test_bpf_xdp_adjust_head_minus_100(void* ctx)
 {
     if (bpf_xdp_adjust_head(ctx, -100) < 0) {
         return XDP_DROP;
