284f727b425a982902190eee876048edd5e19b75
Alan Jowett <alan.jowett@microsoft.com>
2024-04-13 10:37:00 -0700

    Return XDP_PASS on success not 0
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- bpf/xdp.c
+++ bpf/xdp.c
@@ -3,37 +3,37 @@
 
 #include "bpf.h"
 
-SEC("xdp/baseline") int test_xdp_baseline(void** ctx) { return 0; }
+SEC("xdp/baseline") int test_xdp_baseline(xdp_md_t* ctx) { return XDP_PASS; }
 
 // Test cases for bpf_xdp_adjust_head
 
-SEC("xdp/test_bpf_xdp_adjust_head_0") int test_bpf_xdp_adjust_head_0(void** ctx)
+SEC("xdp/test_bpf_xdp_adjust_head_0") int test_bpf_xdp_adjust_head_0(xdp_md_t* ctx)
 {
     if (bpf_xdp_adjust_head(ctx, 0) < 0) {
-        return -1;
+        return XDP_DROP;
     }
-    return 0;
+    return XDP_PASS;
 }
 
-SEC("xdp/test_bpf_xdp_adjust_head_plus_100") int test_bpf_xdp_adjust_head_plus_100(void** ctx)
+SEC("xdp/test_bpf_xdp_adjust_head_plus_100") int test_bpf_xdp_adjust_head_plus_100(xdp_md_t* ctx)
 {
     if (bpf_xdp_adjust_head(ctx, 100) < 0) {
-        return -1;
+        return XDP_DROP;
     }
 
     if (bpf_xdp_adjust_head(ctx, -100) < 0) {
-        return -1;
+        return XDP_DROP;
     }
-    return 0;
+    return XDP_PASS;
 }
 
-SEC("xdp/test_bpf_xdp_adjust_head_minus_100") int test_bpf_xdp_adjust_head_minus_100(void** ctx)
+SEC("xdp/test_bpf_xdp_adjust_head_minus_100") int test_bpf_xdp_adjust_head_minus_100(xdp_md_t* ctx)
 {
     if (bpf_xdp_adjust_head(ctx, -100) < 0) {
-        return -1;
+        return XDP_DROP;
     }
     if (bpf_xdp_adjust_head(ctx, 100) < 0) {
-        return -1;
+        return XDP_DROP;
     }
-    return 0;
+    return XDP_PASS;
 }
