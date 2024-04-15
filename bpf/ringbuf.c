68b5f1920e88574bff7b8bb8ed668fb4a05a1549
Gianni Trevisiol <gtrevi@users.noreply.github.com>
2024-04-15 11:19:41 -0700

    buffer to separate map
    Signed-off-by: Gianni Trevisiol <gtrevi@users.noreply.github.com>

--- bpf/ringbuf.c
+++ bpf/ringbuf.c
@@ -14,13 +14,26 @@
 struct
 {
     __uint(type, BPF_MAP_TYPE_RINGBUF);
-    __uint(max_entries, MAX_RECORDS * RECORD_SIZE);
+    __uint(max_entries, MAX_RECORDS* RECORD_SIZE);
 } map SEC(".maps");
+
+struct
+{
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, int);
+    __uint(value_size, RECORD_SIZE);
+} map2 SEC(".maps");
 
 SEC("sockops/bpf_ringbuf_output") int output(void* ctx)
 {
-    char msg[RECORD_SIZE] = {0};
-    if (bpf_ringbuf_output(&map, msg, sizeof(msg), 0) < 0) {
+    void* msg;
+    uint32_t key = 0;
+    msg = bpf_map_lookup_elem(&map2, &key);
+    if (msg == NULL) {
+        return 1;
+    }
+    if (bpf_ringbuf_output(&map, msg, RECORD_SIZE, 0) < 0) {
         return 1;
     } else {
         return 0;
