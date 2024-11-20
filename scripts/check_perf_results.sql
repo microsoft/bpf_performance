6e13922af07ce84e18cbe0d5dc6ab83ffe6e84ef
Alan Jowett <alan.jowett@microsoft.com>
2024-11-20 14:16:43 -0800

    Parameterize script
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- scripts/check_perf_results.sql
+++ scripts/check_perf_results.sql
@@ -8,14 +8,17 @@ and returns the results that are 2 standard deviations away from the mean.
 The script uses the following parameters:
 - platform: The platform to filter the results by.
 - max_sigma: The number of standard deviations away from the mean to consider a regression.
+- look_back: The number of days to look back for results. Format is 'X days'.
+- repository: The repository to filter the results by.
 */
+
 WITH samples AS (
     SELECT metric, value, "timestamp",
            ROW_NUMBER() OVER (PARTITION BY metric ORDER BY "timestamp" DESC) AS row_num
     FROM benchmarkresults
-    WHERE platform = 'Windows 2019'
-      AND repository = 'microsoft/ebpf-for-windows'
-      AND "timestamp" >= NOW() - INTERVAL '30 days'
+    WHERE platform = :'platform'
+      AND repository = :'repository'
+      AND "timestamp" >= NOW() - INTERVAL :'look_back'
 ),
 stats AS (
     SELECT metric,
@@ -23,8 +26,8 @@ stats AS (
            STDDEV(value) as stddev_value
     FROM benchmarkresults
     WHERE platform = :'platform'
-      AND repository = 'microsoft/ebpf-for-windows'
-      AND "timestamp" >= NOW() - INTERVAL '30 days'
+      AND repository = :'repository'
+      AND "timestamp" >= NOW() - INTERVAL :'look_back'
     GROUP BY metric
 )
 SELECT samples.timestamp, samples.metric, samples.value, stats.mean_value, stats.stddev_value
